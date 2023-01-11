bd1be8172b94ee7b1a696b395b4efe81a0e50a81
sbeers <s.beers@student.tue.nl>
2023-01-11 20:34:03 +0100

    added wrapper node

--- launchers/default.sh
+++ launchers/default.sh
@@ -12,9 +12,16 @@ dt-launchfile-init
 # NOTE: Use the variable DT_REPO_PATH to know the absolute path to your code
 # NOTE: Use `dt-exec COMMAND` to run the main process (blocking process)
 
-# launching app
-dt-exec echo "This is an empty launch script. Update it to launch your application."
 
+# ----------------------------------------------------------------------------
+# YOUR CODE ABOVE THIS LINE
+
+
+# launching app
+#dt-exec echo "This is an empty launch script. Update it to launch your application."
+dt-exec Xvfb :1 -screen 0 1024x768x24 -ac +extension GLX +render -noreset
+export DISPLAY=:1
+dt-exec rosrun simulator_wrapper simulator_wrapper.py
 
 # ----------------------------------------------------------------------------
 # YOUR CODE ABOVE THIS LINE
