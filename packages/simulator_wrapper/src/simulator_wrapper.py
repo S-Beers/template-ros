c9cc33e469768bbfd4be61c7abd7d44a81a6734a
sbeers <s.beers@student.tue.nl>
2023-01-12 09:52:10 +0100

    added dependencies and simulator_help_files

--- packages/simulator_wrapper/src/simulator_wrapper.py
+++ packages/simulator_wrapper/src/simulator_wrapper.py
@@ -1,80 +1,110 @@
+import os
 import rospy
 from duckietown.dtros import DTROS, NodeType
-from sensor_msgs.msg import CompressedImage, Image
+from sensor_msgs.msg import CompressedImage
 from duckietown_msgs.msg import WheelsCmdStamped
 import gym_duckietown
 from gym_duckietown.simulator import Simulator
-from cv_bridge import CvBridge
 import cv2
-
-# Code is taken partially from:
-# https://github.com/ysli16/duckietown_simulatorwrapper/blob/v2/packages/simulatorwrapper/src/simulatorwrapper.py
-
-
-
-def init():
-    env = Simulator(
-        seed=123,  # random seed
-        map_name="loop_empty",
-        max_steps=500001,  # so it doen' t reset
-        domain_rand=0,
-        camera_width=640,
-        camera_height=480,
-        accept_start_angle_deg=5,
-        full_transparency=True,
-        distortion=True,
-    )
-    return env
+from cv_bridge import CvBridge
+VEHICLE_NAME = os.environ['VEHICLE_NAME']
 
 
 class ROSWrapper(DTROS):
+    '''
+    Class that creates a wrapper between the duckietown simulator and the ROS
+    interface of a fake robot simulated in the host. The node serves as interface
+    for the following parts:
+        - Subscribes to the wheels command topic and transforms it into actions in simulation
+        - Publishes the rendered observations in the simulator to the camera image topic
+    '''
 
     def __init__(self, node_name):
-
         # initialize the DTROS parent class
         super(ROSWrapper, self).__init__(node_name=node_name, node_type=NodeType.GENERIC)
-        self.bridge = CvBridge()
 
-        self.pub = rospy.Publisher('/fakebot/camera_node/image/compressed', CompressedImage, queue_size=1)
-        self.sub = rospy.Subscriber('/fakebot/wheels_driver_node/wheels_cmd', WheelsCmdStamped, self.callback)
-        self.action = [0.1, 0.1]
+        # construct rendered observation publisher
+        self.camera_pub = rospy.Publisher(
+            '~/' + VEHICLE_NAME + '/camera_node/image/compressed',
+            CompressedImage,
+            queue_size=1
+        )
 
-    def imagepublisher(self, observation):
-        # rate = rospy.Rate(5)
-        b, g, r = cv2.split(observation)
-        img = cv2.merge([r, g, b])
+        # Construct subscriber for wheel_cmd topic
+        self.wheels_cmd_sub = rospy.Subscriber(
+            '~/' + VEHICLE_NAME + '/wheels_driver_node/wheels_cmd',
+            WheelsCmdStamped,
+            self.wheels_cmd_cb,
+            queue_size=1
+        )
 
-        # img_msg=self.bridge.cv2_to_compressed_imgmsg(observation)
-        img_msg = self.bridge.cv2_to_compressed_imgmsg(img)
-        img_msg.header.stamp = rospy.Time.now()
+        # Initialize the simulator
+        self.env = Simulator(
+            seed=123,  # random seed
+            map_name="loop_empty",
+            max_steps=500001,  # we don't want the gym to reset itself
+            domain_rand=0,
+            camera_width=640,
+            camera_height=480,
+            accept_start_angle_deg=4,  # start close to straight
+            full_transparency=True,
+            distortion=True,
+        )
+        # Create action variable
+        self.action = [0.0, 0.0]
 
-        # rospy.loginfo("Get an image...")
-        self.pub.publish(img_msg)
-        # rospy.loginfo("Published to topic camera_node/image/compressed")
-        # rate.sleep()
+    def wheels_cmd_cb(self, msg):
+        '''
+        Callback that maps wheel commands into actions in the simulator.
+            Creates the simulated robot actions based on the received command
+            through the main topic in the duckietown infrastructure.
+            Args:
+                msg (WheelsCmdStamped): Velocity command
+        '''
 
-    def callback(self, wheelcommand):
-        rospy.loginfo("get wheel command")
-        self.action = [wheelcommand.vel_left, wheelcommand.vel_right]
+        vel_left = msg.vel_left
+        vel_right = msg.vel_right
+
+        self.action = [vel_left, vel_right]
+
+    def run(self):
+        '''
+        Continuously run the wrapper to map ROS interface with simulator.
+            This method runs the node, and begins an iterative process where the
+            image is retrieved from the simulator and published in the camera image topic,
+            and the wheels command received by subscribing to the wheels_cmd topic is translated
+            into actions executed in the simulator. The process iterates until the node is terminated
+        '''
+
+        while not rospy.is_shutdown():
+
+            # Update the simulator
+            # the action message is updated every time the wheels_cmd_cb is called
+            # that is, every time a message arrives.
+            observation, reward, done, misc = self.env.step(self.action)
+            # Render the new state
+            self.env.render()
+
+            # Set again action to stop, in case no more messages are being received
+            self.action = [0.0, 0.0]
+
+            if done:
+                # Reset the simulator when the robot goes out of the road.
+                self.env.reset()
+
+            # Transalte the observation into a CompressedImage message to publish it
+            bgr_obs = cv2.cvtColor(observation, cv2.COLOR_RGB2BGR)
+            bridge = CvBridge()
+            img_msg = bridge.cv2_to_compressed_imgmsg(cvim=bgr_obs, dst_format="jpg")
+
+            # Publish the image in the /image/compressed topic
+            self.camera_pub.publish(img_msg)
 
 
 if __name__ == '__main__':
-    # initialize simulator
-    env = init()
-
     # create the node
-    node = ROSWrapper(node_name='simulatorwrapper')
-    rate = rospy.Rate(50)
-    # start simulation
-    while True:
-
-        action = node.action
-        observation, reward, done, misc = env.step(action)
-        node.imagepublisher(observation)
-        env.render()
-        if done:
-            env.reset()
-        rate.sleep()
-
-        # keep spinning
-    # rospy.spin()
+    node = ROSWrapper(node_name='ros_wrapper_node')
+    # run node
+    node.run()
+    # keep spinning
+    rospy.spin()
